faf9ee7fb795f7e6d0068a0bab657e6e1288b323
a4livet <a4livet@enib.fr>
2017-11-08 15:34:48 +0100

    "Test commit"

---
+++ app/src/main/java/com/sailcoach/sailcoach/Activities/ChooseBoatActivity.java
@@ -0,0 +1,15 @@
+package com.sailcoach.sailcoach.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.sailcoach.sailcoach.R;
+
+public class ChooseBoatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_boat);
+    }
+}
